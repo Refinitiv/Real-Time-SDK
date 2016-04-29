eae5af80dcf38d43aba2b7ff8dd541f916205a41
Tony Louras <tony.louras@thomsonreuters.com>
2016-04-29 13:20:51 -0500

    add HTML docs

---
+++ Java/Ema/Docs/HTML/EMA_Training_Examples/wwhdata/common/files.js
@@ -0,0 +1,39 @@
+function  WWHBookData_Files(P)
+{
+P.fA("100 Series Examples","EMAJ_Readmes.1.01.html");
+P.fA("100__MarketPrice__Streaming","EMAJ_Readmes.1.02.html");
+P.fA("101__MarketPrice__QosPriority","EMAJ_Readmes.1.03.html");
+P.fA("102__MarketPrice__Snapshot","EMAJ_Readmes.1.04.html");
+P.fA("110__MarketPrice__FileConfig","EMAJ_Readmes.1.05.html");
+P.fA("120__MarketPrice__FieldListWalk","EMAJ_Readmes.1.06.html");
+P.fA("130__MarketPrice__UserDisp","EMAJ_Readmes.1.07.html");
+P.fA("140__MarketByOrder__Streaming","EMAJ_Readmes.1.08.html");
+P.fA("200 Series Examples","EMAJ_Readmes.1.09.html");
+P.fA("200__MarketPrice__Streaming","EMAJ_Readmes.1.10.html");
+P.fA("210__MarketByOrder__Streaming","EMAJ_Readmes.1.11.html");
+P.fA("220__MarketByPrice__PrivateStream","EMAJ_Readmes.1.12.html");
+P.fA("230__MarketByOrder__SwitchEntryAction","EMAJ_Readmes.1.13.html");
+P.fA("240__MarketPrice__RippleFields","EMAJ_Readmes.1.14.html");
+P.fA("250__Custom__Opaque","EMAJ_Readmes.1.15.html");
+P.fA("270__SymbolList","EMAJ_Readmes.1.16.html");
+P.fA("280__MarketPrice__Performance","EMAJ_Readmes.1.17.html");
+P.fA("290__Custom__Decode","EMAJ_Readmes.1.18.html");
+P.fA("291__Custom__DownCast","EMAJ_Readmes.1.19.html");
+P.fA("300 Series Examples","EMAJ_Readmes.1.20.html");
+P.fA("300__MarketPrice__Close","EMAJ_Readmes.1.21.html");
+P.fA("301__MarketPrice__PriorityChange","EMAJ_Readmes.1.22.html");
+P.fA("310__MarketPrice__Rmtes","EMAJ_Readmes.1.23.html");
+P.fA("320__Custom__GenericMsg","EMAJ_Readmes.1.24.html");
+P.fA("330__Login__Streaming","EMAJ_Readmes.1.25.html");
+P.fA("331__Directory__Streaming","EMAJ_Readmes.1.26.html");
+P.fA("332__Dictionary__Streaming","EMAJ_Readmes.1.27.html");
+P.fA("340__MarketPrice__OnStreamPost","EMAJ_Readmes.1.28.html");
+P.fA("341__MarketPrice__OffStreamPost","EMAJ_Readmes.1.29.html");
+P.fA("350__MarketPrice__PauseAll","EMAJ_Readmes.1.30.html");
+P.fA("370__MarketPrice__Batch","EMAJ_Readmes.1.31.html");
+P.fA("400 Series Examples","EMAJ_Readmes.1.32.html");
+P.fA("400__Custom__NestedMessaging","EMAJ_Readmes.1.33.html");
+P.fA("410__MarketPrice__HorizontalScaling","EMAJ_Readmes.1.34.html");
+P.fA("420__MarketPrice__AdminDomainConfig","EMAJ_Readmes.1.35.html");
+P.fA("422__MarketPrice__ErrorClient","EMAJ_Readmes.1.36.html");
+}
