83ec98d2d48997b2731fcc73b7cd64d14abbc1eb
mayd <may.du@thomsonreuters.com>
2017-08-15 10:00:29 -0500

    ESDK-652 [GITHUB #31] Bug: OmmNiProviderConfigImpl returns null instead of this

--- Java/Ema/Src/main/java/impl/com/thomsonreuters/ema/access/OmmNiProviderConfigImpl.java
+++ Java/Ema/Src/main/java/impl/com/thomsonreuters/ema/access/OmmNiProviderConfigImpl.java
@@ -122,13 +122,13 @@ class OmmNiProviderConfigImpl extends EmaConfigImpl implements OmmNiProviderConf
 	@Override
 	public OmmNiProviderConfig addAdminMsg(ReqMsg reqMsg) {
 		addAdminMsgInt(reqMsg);
-		return null;
+		return this;
 	}
 
 	@Override
 	public OmmNiProviderConfig addAdminMsg(RefreshMsg refreshMsg) {
 		addAdminMsgInt(refreshMsg);
-		return null;
+		return this;
 	}
 	
 	@Override
