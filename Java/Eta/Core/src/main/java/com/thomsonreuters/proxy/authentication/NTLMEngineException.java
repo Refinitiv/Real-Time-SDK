50497af3542fc9bbe92a87a6e97eb216e754cece
Brett Bush <brett.bush@refinitiv.com>
2019-07-17 11:06:57 -0500

    ESDK-3321 update httpcomponents ver:xsions in gradle from 4.5 to 4.5.3

--- Java/Eta/Core/src/main/java/com/thomsonreuters/proxy/authentication/NTLMEngineException.java
+++ Java/Eta/Core/src/main/java/com/thomsonreuters/proxy/authentication/NTLMEngineException.java
@@ -1,21 +1,23 @@
 package com.thomsonreuters.proxy.authentication;
+
 /*
  * ====================================================================
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- *  Licensed to the Apache Software Foundation (ASF) under one or more
- *  contributor license agreements.  See the NOTICE file distributed with
- *  this work for additional information regarding copyright ownership.
- *  The ASF licenses this file to You under the Apache License, Version 2.0
- *  (the "License"); you may not use this file except in compliance with
- *  the License.  You may obtain a copy of the License at
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  * ====================================================================
  *
  * This software consists of voluntary contributions made by many
@@ -25,47 +27,39 @@ package com.thomsonreuters.proxy.authentication;
  *
  */
 
-import org.apache.http.annotation.Immutable;
 import org.apache.http.auth.AuthenticationException;
 
 /**
  * Signals NTLM protocol failure.
- * 
+ *
+ *
  * @since 4.0
  */
-@Immutable
-public class NTLMEngineException extends AuthenticationException
-{
+public class NTLMEngineException extends AuthenticationException {
+
     private static final long serialVersionUID = 6027981323731768824L;
 
-    /**
-     * Instantiates a new NTLM engine exception.
-     */
-    public NTLMEngineException()
-    {
+    public NTLMEngineException() {
         super();
     }
 
     /**
      * Creates a new NTLMEngineException with the specified message.
-     * 
+     *
      * @param message the exception detail message
      */
-    public NTLMEngineException(String message)
-    {
+    public NTLMEngineException(final String message) {
         super(message);
     }
 
     /**
      * Creates a new NTLMEngineException with the specified detail message and cause.
-     * 
+     *
      * @param message the exception detail message
-     * @param cause the <tt>Throwable</tt> that caused this exception,
-     *            or <tt>null</tt> if the cause is unavailable, unknown,
-     *            or not a <tt>Throwable</tt>
+     * @param cause the {@code Throwable} that caused this exception, or {@code null}
+     * if the cause is unavailable, unknown, or not a {@code Throwable}
      */
-    public NTLMEngineException(String message, Throwable cause)
-    {
+    public NTLMEngineException(final String message, final Throwable cause) {
         super(message, cause);
     }
 
