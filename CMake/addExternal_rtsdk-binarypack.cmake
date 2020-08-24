74458ecc72daa134d363c8062f0d85bad4241c6e
Mitchell Kato <mitchell.kato@thomsonreuters.com>
2020-08-24 22:47:08 +0100

    ESDK-4177: Revert real-time naming for github URL

--- CMake/addExternal_rtsdk-binarypack.cmake
+++ CMake/addExternal_rtsdk-binarypack.cmake
@@ -8,7 +8,7 @@
 include(rcdevExternalUtils)
 
 if(NOT rtsdk-binarypack_url)
-	set(rtsdk-binarypack_url "https://github.com/Refinitiv/Elektron-SDK/releases/download/Elektron-SDK-1.5.0.G1/Elektron-SDK-BinaryPack-1.5.0.G1.tar.xz")
+	set(rtsdk-binarypack_url "https://github.com/Refinitiv/Real-Time-SDK/releases/download/Elektron-SDK-1.5.0.G1/Elektron-SDK-BinaryPack-1.5.0.G1.tar.xz")
 endif()
 if(NOT rtsdk-binarypack_hash)
 	# .xz MD5 hash
