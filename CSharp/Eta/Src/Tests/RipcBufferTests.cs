d98bfbad212288af9ab55269f79f499e6ed3789d
Tymur Baksheiev <tymur.baksheiev@lseg.com>
2024-04-18 19:36:52 +0300

    RTSDK-7947. RTSDK C#: Update dependencies with Operational Risk.
    - Additional update of System.IdentityModel.Tokens.Jwt & Microsoft.IdentityModel.Tokens.
    - Updated some transitive dependencies via Microsoft.NET.Test.Sdk update.
    - Updated direct dependencies & fixed minor incompatibilities according to BlackDuck scan.

--- CSharp/Eta/Src/Tests/RipcBufferTests.cs
+++ CSharp/Eta/Src/Tests/RipcBufferTests.cs
@@ -34,17 +34,6 @@ namespace LSEG.Eta.Transports.Tests
         {
         }
 
-        private static void AssertEqual(byte[] expected, byte[] actual)
-        {
-            int i = 0;
-            for (; i < expected.Length && expected[i] == actual[i]; i++)
-                ;
-            if (i < expected.Length)
-            {
-                throw new Xunit.Sdk.TrueException($"expected({expected[i]}) == actual({actual[i]}) at [{i}]", i == expected.Length);
-            }
-        }
-
         [Fact, Category("Unit")]
         public void ReplyStructPassedByReferenceOk()
         {
@@ -139,7 +128,7 @@ namespace LSEG.Eta.Transports.Tests
 
             ByteBuffer actual = new ByteBuffer(request.MessageLength);
             actual.Write(request);
-            AssertEqual(expected, actual.Contents);
+            Assert.Equal(expected, actual.Contents);
         }
 
         [Fact, Category("Unit")]
@@ -233,14 +222,7 @@ namespace LSEG.Eta.Transports.Tests
 
             byte[] actual = buffer.Contents;
 
-            try
-            {
-                Assert.Equal(expected, actual);
-            }
-            catch(Exception)
-            {
-                throw;
-            }
+            Assert.Equal(expected, actual);
         }
 
         [Fact, Category("Unit")]
@@ -329,7 +311,7 @@ namespace LSEG.Eta.Transports.Tests
             buffer.Write(reply);
 
             byte[] actual = buffer.Contents;
-            AssertEqual(expected, actual);
+            Assert.Equal(expected, actual);
         }
 
         [Fact, Category("Unit")]
