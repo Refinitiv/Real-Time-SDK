0e6f6f9f66db7a56fe759d455b0346d4f3ee7e08
Oleksii Fohol <oleksii.fohol@lseg.com>
2024-12-02 07:26:00 -0600

    RTSDK-9255 Uptick to RTSDK-2.2.3.L1 or EMA/ETA 3.8.3.L1 C# 3.3.1.L1

--- CMake/rtsdkInfo.cmake
+++ CMake/rtsdkInfo.cmake
@@ -12,14 +12,14 @@ unset(rtsdk_DEPENDS_LIST)
 #[====================================================================]
 set(rtsdk_INTERNAL_API_VERSION_MAJOR 3)
 set(rtsdk_INTERNAL_API_VERSION_MINOR 8)
-set(rtsdk_INTERNAL_API_VERSION_PATCH 2)
+set(rtsdk_INTERNAL_API_VERSION_PATCH 3)
 set(rtsdk_INTERNAL_API_VERSION_TWEAK 0)
 set(rtsdk_INTERNAL_RELEASE_TWEAK L1)
 set(rtsdk_RELEASE_TYPE rrg)
 
-set( librssl_SO_VERSION 25 )
+set( librssl_SO_VERSION 26 )
 set( librsslVA_SO_VERSION 26 )
-set( libema_SO_VERSION 21 )
+set( libema_SO_VERSION 22 )
 set( librsslJsonConverter_SO_VERSION 2 )
 
 set(Ansi_VERSION_MAJOR 1)
