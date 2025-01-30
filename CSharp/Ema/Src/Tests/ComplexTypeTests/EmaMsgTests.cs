2a8c906e506d9c5efe115eee918a2cc52951fdb8
Tymur Baksheiev <tymur.baksheiev@lseg.com>
2025-01-30 23:44:54 +0200

    RTSDK-9408. Implemented missing tests for encoding XML & JSON payload in RefreshMsg.

--- CSharp/Ema/Src/Tests/ComplexTypeTests/EmaMsgTests.cs
+++ CSharp/Ema/Src/Tests/ComplexTypeTests/EmaMsgTests.cs
@@ -1,8 +1,8 @@
-﻿/*|-----------------------------------------------------------------------------
+/*|-----------------------------------------------------------------------------
  *|            This source code is provided under the Apache 2.0 license
  *|  and is provided AS IS with no warranty or guarantee of fit for purpose.
  *|                See the project's LICENSE.md for details.
- *|           Copyright (C) 2023-2024 LSEG. All rights reserved.     
+ *|           Copyright (C) 2023-2025 LSEG. All rights reserved.
  *|-----------------------------------------------------------------------------
  */
 
@@ -122,7 +122,7 @@ namespace LSEG.Ema.Access.Tests
             LoadEnumTypeDictionary(dictionary);
 
             int[] domainTypes = { (int)DomainType.SOURCE };
-            int[] payloadTypes = { DataTypes.OPAQUE, DataTypes.MAP };
+            int[] payloadTypes = { DataTypes.OPAQUE, DataTypes.XML, DataTypes.JSON, DataTypes.MAP };
             int[] attribTypes = { DataTypes.VECTOR, DataTypes.SERIES };
 
             foreach (int dt in domainTypes)
@@ -1775,6 +1775,7 @@ namespace LSEG.Ema.Access.Tests
                 DataType.DataTypes.VECTOR,
                 DataType.DataTypes.OPAQUE,
                 DataType.DataTypes.XML,
+                DataType.DataTypes.JSON,
                 DataType.DataTypes.ANSI_PAGE };
 
             foreach (var dataType in dataTypes)
