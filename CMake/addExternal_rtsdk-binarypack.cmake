688f921e16156bdd6662b3783e39e5c4fb39e6f8
Vasavi.Levendel <vasavi.levendel@lseg.com>
2025-09-09 12:03:37 -0500

    Update to BinaryPack URL and version

--- CMake/addExternal_rtsdk-binarypack.cmake
+++ CMake/addExternal_rtsdk-binarypack.cmake
@@ -8,15 +8,15 @@
 include(rcdevExternalUtils)
 
 if(NOT rtsdk-binarypack_url)
-	set(rtsdk-binarypack_url "https://github.com/Refinitiv/Real-Time-SDK/releases/download/Real-Time-SDK-2.3.0.L3/RTSDK-BinaryPack-2.3.0.L3.tar.xz")
+	set(rtsdk-binarypack_url "https://github.com/Refinitiv/Real-Time-SDK/releases/download/Real-Time-SDK-2.3.1.L1/RTSDK-BinaryPack-2.3.1.L1.tar.xz")
 endif()
 if(NOT rtsdk-binarypack_hash)
 	# .xz MD5 hash
 	# .tar.xz 
-	set(rtsdk-binarypack_hash "MD5=635c3252f3b60d763fe341713db8bc1a")
+	set(rtsdk-binarypack_hash "MD5=8b42063524932554e508537f76e3af7a")
 endif()
 if(NOT rtsdk-binarypack_version)
-	set(rtsdk-binarypack_version "2.3.0.2")
+	set(rtsdk-binarypack_version "2.3.1.0")
 endif()
 	
 # If the option for using the system installed 
