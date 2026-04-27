d4e91602927c6e1fefe7177f6b8287ff7c4e3932
arthur.yurov <arthur.yurov@lseg.com>
2026-04-27 14:03:47 -0500

    RTSDK-10729 Changed EMAC Unit Test to check new Enum Type Fragment Size default value

--- Cpp-C/Ema/Examples/Test/UnitTest/EmaActiveConfigTest.cpp
+++ Cpp-C/Ema/Examples/Test/UnitTest/EmaActiveConfigTest.cpp
@@ -876,7 +876,7 @@ void EmaActiveConfigTest::OmmIProviderActiveConfigTestDefaultValues(OmmIProvider
 	// Tests default values
 	EXPECT_EQ(ommIProvideractiveServerConfig.getRefreshFirstRequired(), true);
 	EXPECT_EQ(ommIProvideractiveServerConfig.getMaxFieldDictFragmentSize(), 8192);
-	EXPECT_EQ(ommIProvideractiveServerConfig.getMaxEnumTypeFragmentSize(), 12800);
+	EXPECT_EQ(ommIProvideractiveServerConfig.getMaxEnumTypeFragmentSize(), 21000);
 }
 
 TEST_F(EmaActiveConfigTest, OmmIProviderActiveConfigTest)
